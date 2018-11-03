a01fa83703dd6ed2288106c2db66e7ff16420746
zhaopei <zhaopei@mail.bnu.edu.cn>
2018-11-03 14:49:39 +0800

    Generate_Parentheses

---
+++ 22_Generate_Parentheses.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/generate-parentheses/description/
+ * 递归
+ */
+class Solution {
+    public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        backtrack(res, "", 0, 0, n);
+
+        return res;
+    }
+
+    public:
+    void backtrack(vector<string> &res, string cur, int open, int close, int n) {
+        if ((int)cur.length() == 2 * n) {
+            res.push_back(cur);
+            return;
+        }
+        //如果open < n, 表示open还能放，那就放open
+        if (open < n) {
+            backtrack(res, cur + "(", open + 1, close, n);
+        }
+        //在open数量限定下放close
+        if (close < open) {
+            backtrack(res, cur + ")", open, close + 1, n);
+        }
+
+        return;
+    }
+};
+int main() {
+
+    return 0;
+}
