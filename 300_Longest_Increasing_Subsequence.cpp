8da3189805ea78332216ad802c3b202e8288707c
zhaopei <zhaopei@mail.bnu.edu.cn>
2019-01-03 09:49:04 +0800

    300_Longest_Increasing_Subsequence.cpp

---
+++ 300_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> dp(n, 1);
+        int max_length = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    max_length = max(max_length, dp[i]);
+                }
+            }
+        }
+        return max_length;
+    }
+};
+
+int main() {
+    int n;
+    while (cin >> n) {
+        vector<int> nums;
+        for (int i = 0; i < n; i++) {
+            int t;
+            cin >> t;
+            nums.push_back(t);
+        }
+        Solution* solution = new Solution();
+        cout << solution->lengthOfLIS(nums) << endl;
+    }
+    return 0;
+}
