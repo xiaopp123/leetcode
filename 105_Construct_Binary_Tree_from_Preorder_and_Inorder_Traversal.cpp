bc40cc398dcd3747717130d7e6c9750c27215275
zhaopei <zhaopei@mail.bnu.edu.cn>
2019-07-20 10:09:37 +0800

    根据前序中序求树

--- 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
+++ 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -13,9 +13,32 @@ struct TreeNode {
 
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    TreeNode* dfs(vector<int>& preorder, int k, int i, int j,  vector<int>& inorder) {
+        if (k < 0 || k > (int)preorder.size() - 1) {
+            return NULL;
+        }
+        if (i > j) {
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(preorder[k]);
+        int t = i;
+        while (t <= j) {
+            if (inorder[t] == preorder[k]) {
+                break;
+            }
+            t++;
+        }
 
+        root->left = dfs(preorder, k + 1, i, t - 1, inorder);
+        root->right = dfs(preorder, k + t - i + 1, t + 1, j, inorder);
+
+        return root;
     }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return dfs(preorder, 0, 0, inorder.size() - 1, inorder);
+    }
+
 };
 
 int main () {
