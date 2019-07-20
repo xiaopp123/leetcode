f28f22c2af880c9469174aaf50e5ac34fbdc99af
zhaopei <zhaopei@mail.bnu.edu.cn>
2019-07-20 10:22:08 +0800

    求一个数组能围成的最大矩形面积

--- 11_Container_With_Most_Water.cpp
+++ 11_Container_With_Most_Water.cpp
@@ -4,6 +4,23 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+class Solution {
+    public:
+    int maxArea(vector<int>& height) {
+        int area = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        while (left < right) {
+            area = max(area, (right - left) * min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return area;
+    }
+};
 
 /**
  * https://leetcode.com/problems/container-with-most-water/description/
