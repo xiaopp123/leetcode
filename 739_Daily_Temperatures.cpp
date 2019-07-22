d880108ddbeb3549e05c91c2f3c43b3cc20647f2
zhaopei <zhaopei@mail.bnu.edu.cn>
2019-07-22 22:48:07 +0800

    用单调栈求后面第一个比它大的数

--- 739_Daily_Temperatures.cpp
+++ 739_Daily_Temperatures.cpp
@@ -4,17 +4,20 @@ using namespace std;
 class Solution {
     public:
     vector<int> dailyTemperatures(vector<int>& T) {
-        stack<int> st;
+        if (T.empty()) {
+            return {};
+        }
         int n = T.size();
-        vector<int> res(n);
+        vector<int> res(n, 0);
+        stack<int> st;
         for (int i = n - 1; i >= 0; i--) {
             while (!st.empty() && T[i] >= T[st.top()]) {
                 st.pop();
             }
-            if (!st.empty()) {
-                res[i] = st.top() - i;
-            } else {
+            if (st.empty()) {
                 res[i] = 0;
+            } else {
+                res[i] = st.top() - i;
             }
             st.push(i);
         }
