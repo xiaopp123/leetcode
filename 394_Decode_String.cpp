84aea64a21b39ea65d1de066b520ae9a4f174736
zhaopei <zhaopei@mail.bnu.edu.cn>
2019-07-23 22:53:17 +0800

    双栈或递归对序列展开

--- 394_Decode_String.cpp
+++ 394_Decode_String.cpp
@@ -1,14 +1,53 @@
 #include "heads.h"
 
 using namespace std;
+class Solution {
+public:
+    string decodeString(string s) {
+    }
+};
 
 class Solution {
 public:
     string decodeString(string s) {
         int k = 0;
-        //k是引用的
         return dfs(s, k);
     }
+    string dfs(string s, int & k) {
+        string res = "";
+        while (k < (int)s.length()) {
+            if ((s[k] >= 'a' && s[k] <= 'z') || (s[k] >= 'A' && s[k] <= 'Z')) {
+                res += s[k++];
+            } else if (s[k] == ']') {
+                break;
+            } else {
+                int num = 0;
+                while (s[k] >= '0' && s[k] <= '9') {
+                    num = num * 10 + s[k] - '0';
+                    k++;
+                }
+                k++;
+                string sub_layer = dfs(s, k);
+                //返回时k还在]位置上h
+                k++;
+
+                while (num-- > 0) {
+                    res += sub_layer;
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+/*
+class Solution {
+public:
+    string decodeString(string s) {
+        int k = 0;
+        //k是引用的
+        return dfs(s, k); }
     string dfs(string s, int& k) {
         string res;
         int cnt = 0;
@@ -33,6 +72,7 @@ public:
         return res;
     }
 };
+*/
 
 int main() {
     return 0;
