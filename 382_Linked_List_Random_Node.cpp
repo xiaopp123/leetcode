e3f438110885f11eced9a1a34d2487b641eb4b04
zhaopei <zhaopei@mail.bnu.edu.cn>
2019-07-27 10:12:01 +0800

    等概率随机选择一个数

---
+++ 382_Linked_List_Random_Node.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+
+    /** Returns a random node's value. */
+    int getRandom() {
+        int res = head->val;
+        int i = 2;
+        ListNode* cur = head->next;
+        while (cur) {
+            int j = rand() % i;
+            if (j == 0) {
+                res = cur->val;
+            }
+            i++;
+            cur = cur->next;
+        }
+
+        return res;
+    }
+private:
+    ListNode* head;
+};
