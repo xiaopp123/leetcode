28b2d70c42b5f26e09e8cfb236a4f5c2f068e1ba
zhaopei <1204177244@qq.com>
2019-07-31 10:31:47 +0800

    二分查找，最左边或者最右边

--- 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
+++ 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -4,6 +4,58 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+class Solution {
+    public:
+        vector<int> searchRange(vector<int>& nums, int target) {
+            vector<int> res;
+            if (nums.empty()) {
+                res.push_back(-1);
+                res.push_back(-1);
+                return res;
+            }
+            int left = find_left(nums, target);
+            int right = find_right(nums, target);
+            res.push_back(left);
+            res.push_back(right);
+
+            return res;
+
+        }
+        int find_left(vector<int>& nums, int target) {
+            int l = 0;
+            int r = nums.size() - 1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (nums[mid] >= target) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            if (r + 1 < (int)nums.size() && nums[r + 1] == target) {
+                return r + 1;
+            } else {
+                return -1;
+            }
+        }
+        int find_right(vector<int>& nums, int target) {
+            int l = 0;
+            int r = nums.size() - 1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (nums[mid] <= target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            if (l - 1 >= 0 && nums[l - 1] == target) {
+                return l - 1;
+            } else {
+                return -1;
+            }
+        }
+};
 /**
  * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
  */
