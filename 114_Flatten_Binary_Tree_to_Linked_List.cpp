47d183653ec92fd575a052e374c2753ff1e65ac9
zhaopei <1204177244@qq.com>
2019-08-03 10:52:07 +0800

    二叉树遍历

--- 114_Flatten_Binary_Tree_to_Linked_List.cpp
+++ 114_Flatten_Binary_Tree_to_Linked_List.cpp
@@ -14,6 +14,24 @@ struct TreeNode {
 class Solution {
 public:
     void flatten(TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+        flatten(root->left);
+        flatten(root->right);
+        TreeNode* tmp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        while (root->right) {
+            root = root->right;
+        }
+        root->right = tmp;
+    }
+};
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
         while (root) {
             if (root->left) {
                 TreeNode* pre = root->left;
