025f80af5dc880f6f78e44f56f267c2ec26a563a
zhaopei <1204177244@qq.com>
2019-08-08 22:17:55 +0800

    最长递增子序列

--- 300_Longest_Increasing_Subsequence.cpp
+++ 300_Longest_Increasing_Subsequence.cpp
@@ -13,6 +13,30 @@ class Solution {
             return 0;
         }
         vector<int> dp(n, 1);
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            res = max(dp[i], res);
+        }
+
+        return res;
+    }
+};
+
+/*
+class Solution {
+    public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> dp(n, 1);
         int max_length = 1;
         for (int i = 1; i < n; i++) {
             for (int j = i - 1; j >= 0; j--) {
@@ -25,6 +49,7 @@ class Solution {
         return max_length;
     }
 };
+*/
 
 int main() {
     int n;
